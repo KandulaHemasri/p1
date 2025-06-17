e0f457295f3c593b71708480d470ffe6c0f6b744
KandulaHemasri <114800048+KandulaHemasri@users.noreply.github.com>
2025-06-17 11:49:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,22 +15,17 @@ const validateUser = () => {
 };
 
 const loginForm = () => {
-  const str = `
-    <div style='display:flex'>
-    <div>
+  const str = `<div>
     <h3>Login Form</h3>
     <p id='errorTxt'></p>
+    <label for="email">Email</label>
     <p><input type='text' id='txtEmail1'></p>
+     <label for="password">Password</label>
     <p><input type='password' id='txtPass1'></p>
     <p><button onclick='validateUser()'>Submit</button></p>
     <p><button onclick='registerForm()'>Create Account</button></p>
-    </div>
-    <div>
-    <h3>List of Users</h3>
-    </div>
-    </div>
-    `;
-  root.innerHTML = str;
+    <p><button onclick='showUsers()'>Show Registered Users</button></p>`;
+  root.innerHTML = str + "</div>";
 };
 
 const saveUser = () => {
@@ -48,8 +43,11 @@ const saveUser = () => {
 const registerForm = () => {
   const str = `<div>
     <h3>Registration Form</h3>
+     <label for="name">Name</label>
     <p><input type='text' id='txtName'></p>
+     <label for="email">Email</label>
     <p><input type='text' id='txtEmail'></p>
+     <label for="password">Password</label>
     <p><input type='password' id='txtPass'></p>
     <p><button onclick='saveUser()'>Submit</button></p>
     <p><button onclick='loginForm()'>Already a member? Login here...</button></p>
@@ -64,3 +62,24 @@ const showHome = () => {
     `;
   root.innerHTML = str + "</div>";
 };
+
+const showUsers = () => {
+ if (users.length === 0) {
+    root.innerHTML = "<p>No registered users found.</p><p><button onclick='loginForm()'>Back</button></p>";
+ return;
+ }
+
+let str = `<div>
+<h3>Registered Users</h3>
+<ul>`;
+users.forEach((user, index) => {
+str += `<li>${index + 1}. Name: ${user.name}, Email: ${user.email}</li>`;
+ });
+str += `</ul>
+<p><button onclick='loginForm()'>Back to Login</button></p>
+</div>`;
+
+ root.innerHTML = str;
+};
+
+
